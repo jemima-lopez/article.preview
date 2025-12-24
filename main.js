cba726c52aa1805a837774cd7589b54e8efa2d12
Jemima <jlopezj11@miumg.edu.gt>
2025-12-23 22:09:43 -0600

    commit button

---
+++ main.js
@@ -0,0 +1,10 @@
+
+const shareButton = document.querySelector('.share-button');
+
+
+
+
+shareButton.addEventListener('click', () => {
+    alert('Share button clicked!');
+    document.querySelector('.share-list');
+});
